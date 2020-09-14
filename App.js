ad9471063f10d7b70c10931df4f8cb9a9cac0095
danielsitepu36 <danielsitepu96@gmail.com>
2020-09-14 23:19:18 +0700

    #3 Views, Text, Styles

--- App.js
+++ App.js
@@ -5,9 +5,16 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      style
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Text style={styles.boldText}>Hello, World!</Text>
+      </View>
+      <View style={styles.body}>
+        <Text style={styles.boldText}>
+          Lorem ipsum <Text> BOLD TEST </Text>dolor sit amet.
+        </Text>
+        <Text>Lorem ipsum dolor sit amet.</Text>
+        <Text>Lorem ipsum dolor sit amet.</Text>
+      </View>
     </View>
   );
 }
@@ -18,5 +25,17 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  header: {
+    backgroundColor: "pink",
+    padding: 20,
+  },
+  boldText: {
+    fontWeight: "bold",
+  },
+  body: {
+    backgroundColor: "yellow",
+    padding: 20,
+    // fontWeight: "bold", NOT WORKED CAUSE NO INHERITANCE
   },
 });
