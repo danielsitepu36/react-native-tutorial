1f59be83c762f56444247867f0df4263446ef48d
danielsitepu36 <danielsitepu96@gmail.com>
2020-09-16 21:20:22 +0700

    Back to Initial State

--- App.js
+++ App.js
@@ -5,16 +5,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.boldText}>Hello, World!</Text>
-      </View>
-      <View style={styles.body}>
-        <Text style={styles.boldText}>
-          Lorem ipsum <Text> BOLD TEST </Text>dolor sit amet.
-        </Text>
-        <Text>Lorem ipsum dolor sit amet.</Text>
-        <Text>Lorem ipsum dolor sit amet.</Text>
-      </View>
+      <Text>Default App</Text>
     </View>
   );
 }
@@ -25,17 +16,5 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
-  },
-  header: {
-    backgroundColor: "pink",
-    padding: 20,
-  },
-  boldText: {
-    fontWeight: "bold",
-  },
-  body: {
-    backgroundColor: "yellow",
-    padding: 20,
-    // fontWeight: "bold", NOT WORKED CAUSE NO INHERITANCE
   },
 });
