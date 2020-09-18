99fe56c5e025e010199960196d8de875269a99ed
danielsitepu36 <danielsitepu96@gmail.com>
2020-09-18 14:20:43 +0700

    Chapter3

--- App.js
+++ App.js
@@ -5,7 +5,16 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Default App</Text>
+      <View style={styles.header}>
+        <Text style={styles.boldText}>Hello World!</Text>
+      </View>
+      <View style={styles.body}>
+        <Text style={styles.boldText}>
+          Lorem ipsum dolor <Text>Bold Inherit Test</Text> sit amet
+        </Text>
+        <Text>Lorem ipsum dolor sit amet</Text>
+        <Text>Lorem ipsum dolor sit amet</Text>
+      </View>
     </View>
   );
 }
@@ -16,5 +25,16 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  header: {
+    backgroundColor: "pink",
+    padding: 20,
+  },
+  boldText: {
+    fontWeight: "bold",
+  },
+  body: {
+    backgroundColor: "yellow",
+    padding: 20,
   },
 });
