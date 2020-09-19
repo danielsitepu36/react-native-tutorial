ace14cc159ecf13f8ac7576f789e521eb623f2c5
danielsitepu36 <danielsitepu96@gmail.com>
2020-09-19 16:49:39 +0700

    #9-initial

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, FlatList } from "react-native";
+
+import Header from "./components/header";
 
 export default function App() {
+  const [todos, setTodos] = useState([
+    { text: "main", key: "1" },
+    { text: "mandi", key: "2" },
+    { text: "makan", key: "3" },
+  ]);
+
   return (
     <View style={styles.container}>
-      <Text>Default App</Text>
+      <Header />
+      <View style={styles.content}>
+        {/* {to form} */}
+        <View style={styles.list}>
+          <FlatList
+            data={todos}
+            renderItem={({ item }) => <Text>{item.text}</Text>}
+          />
+        </View>
+      </View>
     </View>
   );
 }
@@ -14,7 +31,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+  },
+  content: {
+    padding: 40,
+  },
+  list: {
+    marginTop: 20,
   },
 });
