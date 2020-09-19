7d5a8c2e5158a4f45f0f4b1eb9d8cf6246f07070
danielsitepu36 <danielsitepu96@gmail.com>
2020-09-19 13:05:00 +0700

    #5-initial

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TextInput } from "react-native";
 
 export default function App() {
+  const [name, setName] = useState("shaun");
+  const [age, setAge] = useState("30");
+
   return (
     <View style={styles.container}>
-      <Text>Default App</Text>
+      <Text>Enter name:</Text>
+      <TextInput
+        multiline
+        style={styles.input}
+        placeholder="e.g. John Doe"
+        onChangeText={(val) => setName(val)}
+      />
+      <Text>Enter age:</Text>
+      <TextInput
+        keyboardType="numeric"
+        style={styles.input}
+        placeholder="e.g. 20"
+        onChangeText={(val) => setAge(val)}
+      />
+      <Text>
+        name: {name}, age:{age}
+      </Text>
     </View>
   );
 }
@@ -16,5 +35,12 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#777",
+    padding: 8,
+    margin: 10,
+    width: 200,
   },
 });
