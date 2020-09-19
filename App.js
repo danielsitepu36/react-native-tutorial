8742d6be375ee5f5439e046082dbdec277e81e96
danielsitepu36 <danielsitepu96@gmail.com>
2020-09-19 22:28:43 +0700

    #14-initial

--- App.js
+++ App.js
@@ -13,6 +13,7 @@ import {
 import Header from "./components/header";
 import TodoItem from "./components/todoItem";
 import AddTodo from "./components/addTodo";
+import Sandbox from "./components/sandbox";
 
 export default function App() {
   const [todos, setTodos] = useState([
@@ -40,6 +41,7 @@ export default function App() {
   };
 
   return (
+    // <Sandbox />
     <TouchableWithoutFeedback
       onPress={() => {
         // console.log("dismissed keyboard");
@@ -71,8 +73,10 @@ const styles = StyleSheet.create({
   },
   content: {
     padding: 40,
+    flex: 1,
   },
   list: {
+    flex: 1,
     marginTop: 20,
   },
 });
